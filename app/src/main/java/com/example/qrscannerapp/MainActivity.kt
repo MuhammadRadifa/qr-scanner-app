d08da6f05506c513b7789dd3e35779d169d1f5bf
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-11 20:45:27 +0800

    add barcode camera

--- app/src/main/java/com/example/qrscannerapp/MainActivity.kt
+++ app/src/main/java/com/example/qrscannerapp/MainActivity.kt
@@ -1,19 +1,70 @@
 package com.example.qrscannerapp
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.example.qrscannerapp.ui.MainApp
 import com.example.qrscannerapp.ui.theme.QRScannerAppTheme
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
 
 class MainActivity : ComponentActivity() {
+    
+    private var text = mutableStateOf("")
+
+    private val barCodeLauncher = registerForActivityResult(ScanContract()){
+            result ->
+        if (result.contents == null){
+            Toast.makeText(this@MainActivity,"Cancelled", Toast.LENGTH_SHORT).show()
+        }else{
+            text.value = result.contents
+        }
+    }
+
+    private fun showCamera(){
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan a QR Code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setOrientationLocked(false)
+
+        barCodeLauncher.launch(options)
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){
+            isGranted ->
+        if(isGranted){
+            showCamera()
+        }
+    }
+
+    private fun checkCameraPermission(context: Context){
+        if(ContextCompat.checkSelfPermission(
+                context, Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED){
+            showCamera()
+        } else if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+            Toast.makeText(context,"Cancelled", Toast.LENGTH_SHORT).show()
+        }else{
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,6 +74,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    //MainApp()
+                    val context = LocalContext.current
                     MainApp()
                 }
             }
