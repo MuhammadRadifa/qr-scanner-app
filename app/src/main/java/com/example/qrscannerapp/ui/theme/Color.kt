6f3909ae907b9c07a9aed4c1f8eaa76de1b1f0a6
MuhammadRadifa <m.difa123456@gmail.com>
2024-02-11 09:00:31 +0800

    add colors

--- app/src/main/java/com/example/qrscannerapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/qrscannerapp/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BluePrimary = Color(0xFF016FFF)
+val BlueSecondary = Color(0xFFF5F6FA)
+val BlueOutline = Color(0xFFCAE1FF)
